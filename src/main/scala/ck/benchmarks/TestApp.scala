90d84a8a81be8f5cf1a0a660d0eaa6460dd6af12
Pierre Ricadat <pierre.ricadat@devsisters.com>
2024-03-07 00:33:42 +0800

    Add kyo

--- src/main/scala/ck/benchmarks/TestApp.scala
+++ src/main/scala/ck/benchmarks/TestApp.scala
@@ -1,10 +1,29 @@
 package ck.benchmarks
 
+import cats.data.Chain
 import ck.benchmarks.Test._
 import ck.benchmarks.ZPureInstances._
+import kyo._
 
 object TestApp extends App {
-  println(testZPure.provideService(Env("config")).runAll(State(2)))
-  println(testMTLChunk[P4].provideService(Env("config")).runAll(State(2)))
-  println(testMTL[P3].run(Env("config"), State(2)))
+  println(
+    Vars.run(
+      Vars.let(State(2))(
+        Vars.let(Chain.empty)(
+          Envs[Env].run(Env("config"))(
+            Aborts[Throwable].run(
+              for {
+                _      <- testKyo
+                state  <- Vars.get[State]
+                events <- Vars.get[Chain[Event]]
+              } yield (events, state)
+            )
+          )
+        )
+      )
+    )
+  )
+//  println(testZPure.provideService(Env("config")).runAll(State(2)))
+//  println(testMTLChunk[P4].provideService(Env("config")).runAll(State(2)))
+//  println(testMTL[P3].run(Env("config"), State(2)))
 }
