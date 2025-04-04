f43f87c7352a905fbf639b905efa46d306853517
Pierre Ricadat <pierre.ricadat@devsisters.com>
2025-04-04 13:45:00 +0900

    Test code

--- src/main/scala/ck/benchmarks/TestApp.scala
+++ src/main/scala/ck/benchmarks/TestApp.scala
@@ -1,24 +1,32 @@
 package ck.benchmarks
 
 import cats.data.Chain
-import ck.benchmarks.Test._
-import ck.benchmarks.ZPureInstances._
-import kyo._
+import ck.benchmarks.Test.{*, given}
+import ck.benchmarks.ZPureInstances.*
+import kyo.*
 
 object TestApp extends App {
   println(
-    Abort
-      .run(
-        Var.run(State(2))(
-          Emit.run(
-            Env.run(Environment("config"))(
-              testKyo.andThen(Var.get[State])
-            )
-          )
-        )
-      )
-      .eval
+    testTurboLift
+      .handleWith(MyReader.handler(Environment("config")))
+      .handleWith(MyState.handler(State(2)))
+      .handleWith(MyWriter.handler)
+      .handleWith(MyError.handler)
+      .run
   )
+//  println(
+//    Abort
+//      .run(
+//        Var.run(State(2))(
+//          Emit.run(
+//            Env.run(Environment("config"))(
+//              testKyo.andThen(Var.get[State])
+//            )
+//          )
+//        )
+//      )
+//      .eval
+//  )
 //  println(testZPure.provideService(Env("config")).runAll(State(2)))
 //  println(testMTLChunk[P4].provideService(Env("config")).runAll(State(2)))
 //  println(testMTL[P3].run(Env("config"), State(2)))
